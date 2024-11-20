6541da0a217da123788753b0c3226ff450437d91
ForgetG <g.forget1999@gmail.com>
2024-11-20 15:05:28 +0100

    added task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
-    return [...set].join('-');
+  if (!startString || typeof startString !== 'string') {
+    return '';
   }
 
-  const result = [...set]
-    .filter((item) => item.startsWith(startString))
-    .map((item) => item.slice(startString.length))
-    .join('-');
-  return result;
+  const result = Array.from(set)
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length));
+
+  return result.join('-');
 }
