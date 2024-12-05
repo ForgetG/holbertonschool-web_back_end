9e9d05c70b96deccff127ecddcd805b99e077890
ForgetG <g.forget1999@gmail.com>
2024-12-05 13:59:49 +0100

    added usage of List(3rd draft)

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,11 +3,12 @@
 import asyncio
 import random
 import importlib
+from typing import List
 
 wait_random = importlib.import_module('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     '''Wait for a random delay and return the result'''
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
