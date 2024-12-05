0b9d6c0b160bc1eb7e79c2d3cee85845c888bdd5
ForgetG <g.forget1999@gmail.com>
2024-12-05 10:22:03 +0100

    added task 3 (first draft)

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Module for to_str function'''
+from typing import Union
+
+
+def to_str(n: Union[int, float]) -> str:
+    '''Converts a float to a string'''
+    return str(n)
